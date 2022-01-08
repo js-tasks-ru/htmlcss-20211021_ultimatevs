38fe3c1e240b0ce542c4be13f64c4543f8e4af3c
Alex Timoshenko <ultimatevs@mail.ru>
2022-01-08 19:01:19 +0300

    bugs fixed and added burger-menu to all pages

---
+++ assets/scripts/burger-menu.js
@@ -0,0 +1,25 @@
+const menu = document.getElementById('burger-menu');
+const menuClose = document.getElementById('burger-menu-close');
+const menuOpen = document.getElementById('burger-menu-open');
+
+if (menuOpen) {
+    menuOpen.addEventListener('click', function() {
+        if (menu) {
+            menu.setAttribute('data-open', '');
+            document.body.overflow = 'hidden';
+        }
+    });
+}
+
+if (menuClose) {
+    menuClose.addEventListener('click', function() {
+        if (menu) {
+            menu.removeAttribute('data-open');
+            document.body.overflow = 'initial';
+        }
+    });
+}
+
+$(document).ready(function() {
+    $("body").removeClass("preload");
+});
